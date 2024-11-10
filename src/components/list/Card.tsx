1e579349b1a40826aeaee1c0f5b2d21dd3594884
Beyza Keser <beyzaaakeser@gmail.com>
2024-11-10 20:40:27 +0300

    update

--- src/components/list/Card.tsx
+++ src/components/list/Card.tsx
@@ -1,12 +1,32 @@
+import { motion } from 'framer-motion';
 import { CarType } from '../../types';
+import Button from '../button';
 import Info from './Info';
+import Modal from '../modal/index';
+import { useState } from 'react';
+
 type Props = {
   car: CarType;
 };
 
 const Card = ({ car }: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
   return (
-    <div className="car-card">
+    <motion.div
+      initial={{ scale: 0.5, opacity: 0 }}
+      whileInView={{ scale: 1, opacity: 1 }}
+      className="car-card group"
+    >
       <h2 className="car-card__content-title">
         {car.make} {car.model}
       </h2>
@@ -23,10 +43,23 @@ const Card = ({ car }: Props) => {
         <img src="/hero.png" className="w-full h-full object-contain" alt="" />
       </div>
 
-      <div className="w-full">
-        <Info car={car} />
+      <div className="w-full ">
+        <div className="group-hover:hidden">
+          <Info car={car} />
+        </div>
+
+        <div className="mt-1 hidden group-hover:flex ">
+          <Button
+            title="Detail"
+            designs="w-full py-[25px] text-white"
+            icon="right-arrow.svg"
+            handleClick={handleOpen}
+          />
+        </div>
       </div>
-    </div>
+
+      <Modal isOpen={isOpen} close={handleClose} car={car} />
+    </motion.div>
   );
 };
 
